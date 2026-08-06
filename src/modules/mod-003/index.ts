946fde51e1e3190654047fd9f0eae7c2669e6ace
HERMETICUM B.C.E. <manuelcoletta1@gmail.com>
2026-08-06 13:31:19 +0200

    feat(mod-003): expose public repository evolution module api

---
+++ src/modules/mod-003/index.ts
@@ -0,0 +1,17 @@
+/**
+ * HERMETICUM B.C.E. S.r.l.
+ * AI JOKER-C2
+ *
+ * MOD-003
+ * Public API
+ */
+
+export * from "./types";
+
+export * from "./snapshot-diff";
+
+export * from "./commit-comparator";
+
+export * from "./change-classifier";
+
+export * from "./repository-evolution.engine";
